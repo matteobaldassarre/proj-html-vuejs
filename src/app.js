61db757bc30188f21af02ac7c25176350dc0a1c7
matteobaldassarre <matteobaldassarre29@gmail.com>
2021-05-11 13:48:44 +0200

    added footer & VueJS Integration

--- src/app.js
+++ src/app.js
@@ -2,7 +2,46 @@ var app = new Vue({
 
     el: '#root',
     data: {
-       
+
+        // Header Main Menu
+        headerMenu: [
+            'HOME',
+            'ABOUT',
+            'SERVICES',
+            'PROCESS',
+            'TEAM',
+            'BLOG'
+        ],
+
+        // Footer About
+        footerAbout: [
+            'The Company',
+            'Institutional',
+            'Social & Events',
+            'Innovation',
+            'Environment',
+            'Technology'
+        ],
+
+        // Footer Services
+        footerServices: [
+            'Audit & Assurance',
+            'Financial Advisory',
+            'Analytics M&A',
+            'Middle Marketing',
+            'Legal Consulting',
+            'Regulatory Risk'
+        ],
+
+        // Footer Support
+        footerSupport: [
+            'Responsibility',
+            'Terms of Use',
+            'About Cookies',
+            'Privacy Policy',
+            'Accessibility',
+            'Information'
+        ]
     },
     methods: {
         
